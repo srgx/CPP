9a5dbb7651c4b4b5b31a86a7589d485d578b3973
Jan Kowalski <yakov85@o2.pl>
2020-01-09 12:30:31 +0100

    Months

--- chapter5.cpp
+++ chapter5.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <array>
+#include <string>
 
 using namespace std;
 
@@ -7,10 +8,16 @@ void exercise1(); void exercise2(); void exercise3(); void exercise4();
 void exercise5(); void exercise6(); void exercise7(); void exercise8();
 void exercise9(); void exercise10();
 
-int main(){
-  exercise5();
-}
 
+const int MONTHS = 12;
+const string MONTH_NAMES[MONTHS] = {
+    "Styczeń", "Luty", "Marzec", "Kwiecień", "Maj", "Czerwiec",
+    "Lipiec" ,"Sierpień", "Wrzesień" ,"Październik", "Listopad", "Grudzień"
+  };
+
+int main(){
+  exercise7();
+}
 
 void exercise1(){
   cout << "Podaj pierwszą liczbę: ";
@@ -26,10 +33,10 @@ void exercise1(){
 
 void exercise2(){
   const int ArSize = 101; array<long double,ArSize> factorials;
-  factorials[0] = factorials[1] = 1;
+  factorials.at(0) = factorials.at(1) = 1;
   for(int i=2;i<ArSize;i++)
-    factorials[i] = i * factorials[i-1];
-  cout << "100! = " << factorials[100] << endl;
+    factorials.at(i) = i * factorials.at(i-1);
+  cout << "100! = " << factorials.at(100) << endl;
 }
 
 void exercise3(){
@@ -57,5 +64,44 @@ void exercise4(){
 }
 
 void exercise5(){
+  int kwoty[MONTHS];
+  for(int i=0;i<MONTHS;i++){
+    cout << "Podaj kwotę za " << MONTH_NAMES[i] << ": "; cin >> kwoty[i];
+  }
+  int suma = 0;
+  for(int i=0;i<MONTHS;i++)
+    suma += kwoty[i];
+  
+  cout << "Łączna sprzedaż: " << suma << endl;
+   
+}
+
+void exercise6(){
+  const int years = 3;
+  
+  int kwoty [years][MONTHS];
+  for(int i=0;i<years;i++){
+    cout << "Rok " << i+1 << endl;
+    for(int j=0;j<MONTHS;j++){
+      cout << "Podaj kwotę za " << MONTH_NAMES[j] << ": "; cin >> kwoty[i][j];
+    }
+  }
+  
+  int suma_lata = 0;
+  for(int i=0;i<years;i++){
+    int suma_rok = 0;
+    for(int j=0;j<MONTHS;j++){
+      suma_rok += kwoty[i][j];
+    }
+    cout << "Sprzedaż za rok " << i+1 << ": " << suma_rok << endl;
+    suma_lata += suma_rok;
+  }
+  
+  cout << "Sprzedaż razem: " << suma_lata << endl;
+}
+
+void exercise7(){
   // ...
 }
+
+
