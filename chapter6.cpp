1b7297bbf7e27306d5ec2d955827d4c887fb8389
Jan Kowalski <yakov85@o2.pl>
2020-01-11 13:11:45 +0100

    Ćwiczenia 6.5, 6.6, 6.7

--- chapter6.cpp
+++ chapter6.cpp
@@ -5,7 +5,7 @@
 using namespace std;
 
 void exercise1(); void exercise2(); void exercise3(); void exercise4();
-void exercise5();
+void exercise5(); void exercise6(); void exercise7(); void exercise8();
 void showmenu();
 
 const int STRSIZE = 40;
@@ -17,8 +17,13 @@ struct zpdw{
   int preferencje;
 };
 
+struct donator{
+  string nazwisko;
+  double kwota;
+};
+
 int main(){
-  exercise5();
+  exercise8();
 }
 
 void exercise1(){
@@ -121,8 +126,80 @@ void exercise4(){
 }
 
 void exercise5(){
+  cout << "Ile zarabiasz('q' aby zakończyć): "; float ile;
+  while(cin>>ile&&ile>0){
+    cout << "Podatek: ";
+      if(ile<=5000){
+        cout << 0;
+      }else if(ile<=15000){
+        cout << 5000*0 + (ile-5000)*0.1;
+      }else if(ile<=35000){
+        cout << 5000*0 + 10000*0.1 + (ile-15000)*0.15;
+      }else{
+        cout << 5000*0 + 10000*0.1 + 20000*0.15 + (ile-35000)*0.2;
+      }
+      cout << endl;
+      cout << "Ile zarabiasz('q' aby zakończyć): ";
+  }
+  cout << "Gotowe\n";
+}
+
+void exercise6(){
+  const int GRANICA = 10000;
+  cout << "Podaj liczbę wpłacających: "; int ile; cin >> ile; cin.get();
+  donator * pt = new donator[ile];
+  
+  for(int i = 0;i<ile;i++){
+    cout << "Donator " << i+1 << endl;
+    cout << "Nazwisko: "; getline(cin,pt[i].nazwisko);
+    cout << "Kwota: "; cin >> pt[i].kwota; cin.get();
+  }
+  
+  cout << "***NASI WSPANIALI SPONSORZY***\n";
+  bool empty = true;
+  for(int i = 0;i<ile;i++){
+    if(pt[i].kwota>=GRANICA){
+      cout << pt[i].nazwisko << endl; empty = false;
+    }
+  }
+  
+  if(empty)
+    cout << "brak\n";
+  
+  empty = true;
+  cout << "**NASI SPONSORZY**\n";
+  for(int i = 0;i<ile;i++){
+    if(pt[i].kwota<GRANICA){
+      cout << pt[i].nazwisko << endl; empty = false;
+    }
+  }
+  
+  if(empty)
+    cout << "brak\n";
+  
+  delete [] pt;
+}
+
+void exercise7(){
+  cout << "Podawaj słowa(q kończy):\n"; string slowo; cin >> slowo;
+  int spol, samo, inne; spol = samo = inne;
+  while(slowo.length()!=1||slowo[0]!='q'){
+    char l = tolower(slowo[0]);
+    if(isalpha(l)){
+      if(l=='a'||l=='e'||l=='o'||l=='u'||l=='i'||l=='y') { samo++; }
+      else { spol++; }
+    }else { inne++; }
+    cin >> slowo;
+  }
+  
+  cout << "Spółgłoski: " << spol << endl;
+  cout << "Samogłoski: " << samo << endl;
+  cout << "Inne: " << inne << endl;
+}
+
+void exercise8(){
   // ...
-  cout << "Naturlandia\n";
+  cout << "Plik tekstowy.\n";
 }
 
 void showmenu(){
