ab83bcadff14c77a480d188d234f54cd47e398e9
Jan Kowalski <yakov85@o2.pl>
2020-01-18 18:38:48 +0100

    Chapter 9

--- chapter9/ex4/main.cpp
+++ chapter9/ex4/main.cpp
@@ -2,6 +2,15 @@
 #include "sales.h"
 
 int main(){
+  const int ArSize=3;
   SALES::Sales pierwszy, drugi;
-  std::cout << "CZE\n";
+  double vals[ArSize] = {12,34,55};
+  setSales(pierwszy);
+  setSales(drugi,vals,ArSize);
+  std::cout << "----\n";
+  showSales(pierwszy);
+  std::cout << "----\n";
+  showSales(drugi);
+  std::cout << "----\n";
+  std::cout << "Koniec\n";
 }
--- chapter9/ex4/sales.cpp
+++ chapter9/ex4/sales.cpp
@@ -5,22 +5,22 @@ namespace SALES{
 
   void setSales(Sales & s, const double ar[],int n){
     int toCopy = n < QUARTERS ? n : QUARTERS;
-    double min,max,sum;
-    sum = 0;
+    
     for(int i=0;i<QUARTERS;i++){
       s.sales[i] = i<toCopy ? ar[i] : 0;
-      sum+=s.sales[i];
     }
     
-    min = max = s.sales[0];
+    double min,max,sum;
+    min = max = sum = s.sales[0];
     for(int i=1;i<QUARTERS;i++){
       if(s.sales[i]<min){ min = s.sales[i]; }
       if(s.sales[i]>max){ max = s.sales[i]; }
+      sum+=s.sales[i];
     }
     
     s.max = max;
     s.min = min;
-    s.average = sum / toCopy;
+    s.average = sum / QUARTERS;
   }
 
   void setSales(Sales & s){
@@ -28,10 +28,27 @@ namespace SALES{
       std::cout << "Podaj wartość za kwartał " << i+1 << ": ";
       std::cin >> s.sales[i];
     }
+    
+    double min, max, sum;
+    min = max = sum = s.sales[0];
+    for(int i=1;i<QUARTERS;i++){
+      if(s.sales[i]<min){ min = s.sales[i]; }
+      if(s.sales[i]>max){ max = s.sales[i]; }
+      sum += s.sales[i];
+    }
+    
+    s.max = max;
+    s.min = min;
+    s.average = sum / QUARTERS;
   }
   
   void showSales(const Sales & s){
-    // ...
+    for(int i=0;i<QUARTERS;i++){
+      std::cout << "Kwartał " << i+1 << ": " << s.sales[i] << std::endl;
+    }
+    std::cout << "Max: " << s.max << std::endl;
+    std::cout << "Min: " << s.min << std::endl;
+    std::cout << "Avg: " << s.average << std::endl;
   }
 }
 
