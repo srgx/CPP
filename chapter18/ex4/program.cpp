e17053e2628969b96a84896d83f5fb95ce879b22
Jan Kowalski <yakov85@o2.pl>
2020-03-18 22:35:45 +0100

    Chapter 18

--- chapter18/ex4/program.cpp
+++ chapter18/ex4/program.cpp
@@ -4,17 +4,7 @@
 #include <iterator>
 #include <algorithm>
 
-template<class T> // klasa funktora definiuje operator()()
-class TooBig
-{
-    private:
-        T cutoff;
-    public:
-        TooBig(const T & t) : cutoff(t) {}
-        bool operator()(const T & v) { return v > cutoff; }
-};
-
-void outint(int n) { std::cout << n << " "; }
+auto outint = [](int n){ std::cout << n << " "; };
 
 int main()
 {
@@ -22,13 +12,9 @@ int main()
     using std::cout;
     using std::endl;
 
-
-    TooBig<int> f100(100); // ograniczenie = 100
-
     int vals[10] = {50, 100, 90, 180, 60, 210, 415, 88, 188, 201};
     list<int> yadayada(vals, vals + 10);
     list<int> etcetera(vals, vals + 10);
-
 
     // w C++11 można alternatywnie użyć:
     // list<int> yadayada = {50, 100, 90, 180, 60, 210, 415, 88, 188, 201};
@@ -38,8 +24,10 @@ int main()
     cout << endl;
     for_each(etcetera.begin(), etcetera.end(), outint);
     cout << endl;
-    yadayada.remove_if(f100);             // używa nazwanego obiektu funkcyjnego
-    etcetera.remove_if(TooBig<int>(200)); // tworzy obiekt funkcyjny
+
+    yadayada.remove_if([](int t){ return [t](int v){ return v > t; }; }(100));
+    etcetera.remove_if([](int t){ return [t](int v){ return v > t; }; }(200));
+
     cout << "Przycięte listy:\n";
     for_each(yadayada.begin(), yadayada.end(), outint);
     cout << endl;
